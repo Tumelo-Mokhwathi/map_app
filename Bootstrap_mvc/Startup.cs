cec0a0b4224b9dd4dcb720c18487e92350cb7346
Tumelo20 <51445929+Tumelo20@users.noreply.github.com>
2019-10-07 11:37:28 +0200

    Map-App

---
+++ Bootstrap_mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap_mvc.Startup))]
+namespace Bootstrap_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
